1c43028dcf0ee2ae5e29cda566e0a1ce1ade4ab7
Jake Novelli <jakenovelli11@gmail.com>
2021-04-26 16:20:25 -0700

    added update roles and remove employees

--- employeeReadData.js
+++ employeeReadData.js
@@ -1,5 +1,4 @@
 const inquirer = require('inquirer');
-const { isNumber } = require('lodash');
 const mysql = require('mysql');
 
 var connection = mysql.createConnection({
@@ -23,30 +22,28 @@ const initQuestion = {
 
 const startDirect = () => {
     inquirer.prompt(initQuestion).then((answers) => {
-        console.log(answers);
         switch (answers.todo) {
             case 'View All Employees':
                 viewAllEmployees();
             break;
-            // case 'Update Employee Role':
-
-
+            case 'Update Employee Role':
+                updateRole();
+            break;
             case 'Add Employee':
                 addEmployee();
             break;
-            // case 'Remove Employee':
-                // removeEmployee();
-                // break;
+            case 'Remove Employee':
+                removeEmployee();
+            break;
             case 'View All Roles': 
                 viewRoles();
             break;
             case 'Add Role':
                 addRole();
             break;
-            // case 'Remove Role': 
-                // removeRole();
-                // break;
-
+            case 'Remove Role': 
+                removeRole();
+            break;
             case 'View All Departments': 
                 viewDepartments();
             break;
@@ -55,7 +52,7 @@ const startDirect = () => {
                 addDepartment();
             break;
             case 'Remove Department':
-                removeDepartment();
+                removeDep();
             break;
             case 'Exit':
                 console.log('Have a Great Day!');
@@ -63,7 +60,74 @@ const startDirect = () => {
         }
     })
 };
-const removeDepartment = () => {
+
+const updateRole = () => {
+    connection.query('SELECT CONCAT (first_name, " ", last_name) AS name, id AS value FROM employee', (err, employees) => {
+        if (err) throw err;
+    connection.query('SELECT title AS name, id AS value FROM role', (err, roles) => {
+        if (err) throw err;
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'Which employee would you like to change the role of?',
+            name: 'id',
+            choices: employees
+        },
+        {
+            type: 'list',
+            message: 'What role would you like to assign to this employee?',
+            name: 'title',
+            choices: roles
+        }
+    ]).then((answers) => {
+        connection.query(`UPDATE employee SET role_id = ${answers.title} WHERE id = ${answers.id}`, (err, res) => {
+            if (err) throw err;
+            console.log('Role Updated!');
+            startDirect();
+        })
+    })
+    })
+    })
+};
+
+const removeEmployee = () => {
+    connection.query('SELECT CONCAT(first_name, " ", last_name) AS name, id AS value FROM employee', (err, employees) => {
+        if (err) throw err;
+    inquirer.prompt({
+        type: 'list',
+        message: 'Which employee would you like to remove?',
+        name: 'id',
+        choices: employees
+    }).then((answer) => {
+        connection.query('DELETE FROM employee WHERE id = ?', (answer.id), (err, res) => {
+            if (err) throw err;
+            console.log('Employee removed!');
+            startDirect();
+        })
+    })
+    })
+};
+
+const removeRole = () => {
+    connection.query('SELECT title AS name FROM role', (err, roles) => {
+        if (err) throw err;
+    inquirer.prompt(
+        {
+            type: 'list',
+            message: 'What role would you like to remove?',
+            name: 'title',
+            choices: roles
+        }).then((answer) => {
+            connection.query('DELETE FROM role WHERE ?', (answer), (err, res) => {
+                if (err) throw err;
+                console.log('Role removed!');
+                startDirect();
+            })
+        })
+    })
+};
+
+const removeDep = () => {
     connection.query('SELECT dep_name AS name FROM department', (err, departments) => {
         if (err) throw err;
         inquirer.prompt({
@@ -80,7 +144,8 @@ const removeDepartment = () => {
             })
         })
     })
-}
+};
+
 const addRole = () => {
     connection.query('SELECT dep_name AS name, id AS value FROM department', (err, departments) => {
         if (err) throw err;
