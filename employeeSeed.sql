571f518ff12d422b14d5f7692b269236cb6b6f8e
Jake Novelli <jakenovelli11@gmail.com>
2021-04-26 20:21:56 -0700

    added foreign keys

--- employeeSeed.sql
+++ employeeSeed.sql
@@ -13,8 +13,9 @@ CREATE TABLE department (
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30),
-    salary DECIMAL(6,3) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
     department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE,
     PRIMARY KEY (id)
 );
 
@@ -24,7 +25,9 @@ CREATE TABLE employee (
     first_name VARCHAR(30),
     last_name  VARCHAR(30),
     role_id INT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
     manager_id INT,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL,
     PRIMARY KEY (id)
 );
 
@@ -35,12 +38,12 @@ VALUES ("Sales"),
 ("Engineering"),
 ("Maintenance");
 
-INSERT INTO roles (title, salary, dep_id) 
-VALUES ("Sales Rep", 90.500, 1), 
-("Sales Supervisor", 120.000, 1), 
-("Software Engineer", 115.000, 3), 
-("Lead Accountant", 140.000, 2),
-("Custodian", 70.000, 4);
+INSERT INTO role (title, salary, department_id) 
+VALUES ("Sales Rep", 90500.00, 1), 
+("Sales Supervisor", 120000.00, 1), 
+("Software Engineer", 115000.00, 3), 
+("Lead Accountant", 140000.00, 2),
+("Custodian", 70000.00, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("John", "Snow", 2, 1), ("Holly", "Smith", 1, 1), ("William", "Broboski", 3, 2), ("Alex", "Hall", 5, 3), ("Margie", "Holland", 4, 4);
